bc4c50f42f215c1e9e1e683b545b6f78cf7a2e03
SeyiRecord <j.oluseyi.adeola.12@aberdeen.ac.uk>
2013-06-10 22:36:42 +0100

    Commit 01

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Work::Application.config.secret_token = 'c2afc43fed3eb905efb2d52d6d016bcb5ef5b1e0d2c7e93404ddccbc3d1ef08df2b9cf9d96d5f5c8ae4d161ea132b78d8e1c6cff72fa7c5483039bbc4e1f0f36'
